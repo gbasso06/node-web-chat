cd984951ead39ebc32b24dff60330cc6529ce2d6
Gustavo Basso <gbasso06@gmail.com>
2017-08-14 09:16:11 -0300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -1,14 +1,48 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
-	console.log('Server is up on port ${PORT}');
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	// socket.emit('newEmail', {
+	// 	from: 'ritinha@invejosas.com',
+	// 	text: 'Te pego na saída falsiane',
+	// 	createAt:123
+	// });
+
+	socket.on('createEmail', (newEmail) => {
+		console.log('createEmail', newEmail);
+	});
+
+	socket.on('disconnect', () => {
+		console.log("Client disconnected");
+	});
+
+	socket.on('createMessage', (newMessage) => {
+
+		socket.emit('newMessage', {
+			from: newMessage.from,
+			text: newMessage.text,
+			createAt:123
+		});
+
+		console.log("createMessage", newMessage);
+	});
+});
+
+server.listen(PORT, () => {
+	console.log(`Server is up on port ${PORT}`);
 });
 
 // console.log(__dirname + '/../public');
