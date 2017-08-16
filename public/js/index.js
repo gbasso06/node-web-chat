e4d9fb0331d38ce27b5d10a16105cd35739e486c
Gustavo Basso <gbasso06@gmail.com>
2017-08-16 09:36:29 -0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,22 +31,32 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function(e) {
 	e.preventDefault();
 
+	var messageTextbox = jQuery('[name=message');
+
 	socket.emit('createMessage', {
 		from: 'User',
-		text: jQuery('[name=message]').val()
+		text: messageTextbox.val()
 	}, function () {
+		messageTextbox.val('');
 
 	});
 });
 
+
+// Location button  listener
+
 var locationButton = jQuery('#send-location');
+
 locationButton.on('click', function () {
 	
 	if(!navigator.geolocation) {
 		return alert('Geolocation not supported by your browser.');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition(function (position){
+		locationButton.removeAttr('disabled').text('Send location');
 
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
@@ -54,6 +64,7 @@ locationButton.on('click', function () {
 		});
 
 	}, function () {
+		locationButton.removeAttr('disabled').text('Send location');
 		alert('Unable to fetch location.');
 	});
 
