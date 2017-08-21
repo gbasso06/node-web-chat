403e78bef1634eefea08a7e59fc47c300f3d2b32
Gustavo Basso <gbasso06@gmail.com>
2017-08-21 10:14:28 -0300

    Add join page that submits to chat.html

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,25 @@
 var socket = io();
 
+function scrollToBottom () {
+	// Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+	console.log(messages);
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function () {
 	console.log('Connected to server');
 });
@@ -21,6 +41,7 @@ socket.on('newMessage', function(message){
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 
@@ -36,7 +57,7 @@ socket.on('newLocationMessage', function (message) {
 	});
 
 	jQuery('#messages').append(html);
-
+	scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
